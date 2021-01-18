5a9ac016612b8868faa2f3bde823c2f50d1928f0
rahulwakse000 <rahulwaske000@gmail.com>
2021-01-18 15:39:01 +0530

    full changes

---
+++ learn/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from django.conf.urls.static import static
+
+from .views import index
+
+
+urlpatterns = [
+    path('', index, name='homepage')
+]
--- learn/views.py
+++ learn/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # from .models import question
 
 
 # Create your views here.
 
+def index(request):
+    return render(request, 'index.html')
