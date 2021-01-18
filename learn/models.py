0869f9fa24abc23a1ed6bec490bb4c33bcf3532a
rahulwakse000 <rahulwaske000@gmail.com>
2021-01-18 14:06:15 +0530

    changes in models

---
+++ learn/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    que = models.CharField(max_length=1000)
+    ans1 = models.CharField(max_length=100)
+    ans2 = models.CharField(max_length=100)
+    ans3 = models.CharField(max_length=100)
+    ans4 = models.CharField(max_length=100)
+    correct_answer = models.CharField(max_length=100, default='')
---
+++ learn/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# from .models import question
+
+
+# Create your views here.
+
